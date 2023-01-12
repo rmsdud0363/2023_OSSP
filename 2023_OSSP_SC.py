4c876dcdd6c43b28fac8a39ae7b2fa8fc382de21
rmsdud0363 <rmsdud0363@naver.com>
2023-01-12 18:19:12 +0900

    Update OSSP 01

--- 2023_OSSP_SC.py
+++ 2023_OSSP_SC.py
@@ -1,1 +1,12 @@
 # 2023_OSSP_SourceCode
+
+from PIL import Image
+import os, glob, numpy as np
+from sklearn.model_selection import train_test_split
+import os, glob, numpy as np
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
